51d20358ef0e9329e8abcf1561d0961fff5c36fa
vvpatil <vicky_vishalpatil@hotmail.com>
2019-06-04 16:15:23 +0530

    Add files via upload
    This plugin checks if any of the files from the specified directory has breached the aging threshold. it checks the individual file's created time against current system time.
    If the maximum age of any file is exceeded, a critical message is returned.
    
    It can check for a local as well as a shared path on your system.
    
    Tested on Windows 2k8, 2k3 servers
    1. Copy the plugin in scripts directory of your nsclient folder 
    2. modify the Path & age in the plugin 
    3. add the command alias under "/settings/external scripts/wrapped scripts"

---
+++ check_fileage.ps1
@@ -0,0 +1,30 @@
+################################################################################
+## This plugin pulls all files in each specified directory, and checks their  ##
+## created time against the current time. If the maximum age of any file is   ##
+## exceeded, a critical message is returned as appropriate.                   ##
+## Plugin by Vishal Vilas Patil  - vishal.patil2906@gmail.com		      ##
+################################################################################
+
+# Change the path & adjust the days/hours/mins as per your requirement
+$fullPath = "DIRECTORY_PATH"
+$numdays = 0
+$numhours = 0
+$nummins = 45
+
+function ShowOldFiles($path, $days, $hours, $mins)
+{
+    $files = @(get-childitem $path -include *.* -recurse | where {($_.LastWriteTime -lt (Get-Date).AddDays(-$days).AddHours(-$hours).AddMinutes(-$mins)) -and ($_.psIsContainer -eq $false)})
+    if ($files -ne $NULL)
+    {
+        for ($idx = 0; $idx -lt $files.Length; $idx++)
+        {
+            $file = $files[$idx]
+            write-host ("Old: " + $file.Name);exit 2
+        }
+    }
+else {
+write-host ("No files older then 45mins"); exit 0
+}
+}
+
+ShowOldFiles $fullPath $numdays $numhours $nummins
